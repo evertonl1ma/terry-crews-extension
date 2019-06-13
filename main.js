0cf66fd5ad359d20b2f4c033dc099e6e29567f12
Everton Lima <ever-tonlima@hotmail.com>
2019-06-13 00:07:49 -0300

    Ajustes

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-let images = [...document.images];
+(() => {
+	changeImage = () => {
+		console.log('Java é uma merda')
+		let images = [...document.images];
+		setTimeout(() => {
+			changeImage();
+			images.forEach(image => image.src = 'https://i.pinimg.com/originals/29/06/84/290684438fc012a5168898b7cc597b02.jpg')
+		}, 3000)
+	}
 
-images.forEach(image => image.src = 'https://i.pinimg.com/originals/29/06/84/290684438fc012a5168898b7cc597b02.jpg')
+	changeImage();
+
+})()
+	
